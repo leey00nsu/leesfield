3610695e839655e5a59da4070479cddd0df66d65
leey00nsu <dbstndla1212@naver.com>
2026-01-10 16:13:47 +0900

    feat(#13): 모델 관리 화면 추가

---
+++ src/app/(dashboard)/model/page.tsx
@@ -0,0 +1,5 @@
+import { ModelManagementScreen } from "@/screens/model-management/ui/model-management-screen";
+
+export default function ModelManagementPage() {
+  return <ModelManagementScreen />;
+}
